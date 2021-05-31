4ab869860792b72ef6a30caec6be5d66fa5716b3
zhaozhenghao <975106543@qq.com>
2021-05-31 20:28:36 +0800

    props

--- react/clidemo1/src/App.js
+++ react/clidemo1/src/App.js
@@ -1,93 +1,23 @@
-// import './App.css';
 import React from 'react';
-
+import Demo1 from './propsReact/demo1'
 /**
- * 评论列表demo
+ * props 传值
+ * 父传子
+ * 子传父
+ * 兄弟组件
  */
 class App extends React.Component {
-  constructor() {
-    super()
-
-    // 创建ref
-    this.state = {
-      comments: [
-        {id:1,name:'jeck',content: '大家撒法和'},
-        {id:2,name:'zhanm',content: '大地方撒地方家撒法和'},
-        {id:3,name:'boxfd',content: 'dsfasdf'},
-      ],
-      userName:'',
-      userContent:''
-    }
+  state = {
+    name:'比亚迪'
   }
-  getTxt = ()=>{
-    console.log(this.txtRef.current.value)
-  }
-
-  listObj = () =>{
-    const {comments} = this.state;
-    if(comments.length < 1){
-      return <div>暂无评论</div>
-    }else{
-      return (<ul>
-        {
-          comments.map((item) =>(
-            <li key={item.id}>
-              <p>评论人: {item.name}</p>
-              <p>评论内容: <br/> {item.content}</p>
-            </li>
-          ))
-        }
-        
-      </ul>)
-    }
-  }
-
-  onhandleinput = (e) => {
-    const {name, value} = e.target;
-    this.setState({
-      [name]:value
-    })
-  }
-
-  submit = () =>{
-    const {userName, userContent,comments} = this.state;
-
-    // 校验
-    if(userName.trim() === '' || userContent.trim() === ''){
-      alert('输入框不能为空')
-      return;
-    }
-
-    const newObj = {
-      id:Math.ceil(Math.random() * Math.floor(Math.random() * 100)),
-      name:userName,
-      content:userContent
-    }
-
-    const newComponents = [newObj,...comments];
-    this.setState({
-      comments:newComponents,
-      userName:'',
-      userContent:''
-    })
-
-
-
-  }
-
   render() {
     return (
-      <div style = {{border:'1px solid #666',width:'auto'}}>
-        <div>
-          评论人： <input type="text" value={this.state.userName} name ='userName' onChange={this.onhandleinput} />
-        </div>
-        <div>
-          评论富文本框： <textarea type="textarea" value= {this.state.userContent} name ='userContent' onChange={this.onhandleinput}></textarea>
-        </div>
-        <div>
-          <button onClick = {this.submit} >提交</button>
-        </div>
-        <div>{this.listObj()}</div>
+      <div>
+        <Demo1 name
+        fns={()=>console.log('我是函数')}
+        tag={<p>我是标签</p>}
+          
+        ></Demo1>
       </div>
     )
   }
---
+++ react/clidemo1/src/propsReact/demo1.js
@@ -0,0 +1,26 @@
+import React from 'react';
+/**
+ * props 只读不可修改
+ * 使用类组件 如果写了构造函数，应该将props 传递给 super(),否则无法在构造函数中获取props
+ */
+class demo1 extends React.Component {
+    constructor(props){
+        super(props)
+        // console.log(props)
+        props.fns()
+    }
+    render(){
+        console.log(this.props)
+        return(
+            <div>
+                我叫：{this.props.name}
+                <div>
+                    props.tag:<br/>
+                    {this.props.tag}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default demo1
