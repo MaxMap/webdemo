dc18c13bd46293be554c96e5d789a3c3c6db5d2c
zhaozhenghao <975106543@qq.com>
2021-07-08 19:09:57 +0800

    vite

--- vite/vite-project/src/App.vue
+++ vite/vite-project/src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  {{obj.count}}
+  <h1>{{refs}}</h1>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<!-- <script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+import {reactive} from 'vue'
+const obj = reactive({ count: 0 })
+</script> -->
+<script>
+import {ref,reactive } from 'vue'
+export default {
+  setup(props) {
+    console.log(props.title)
+    const refs = ref(123)
+    const obj = reactive({ count: 5 })
+    return {
+      refs,
+      obj
+      }
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #999;
-  margin-top: 60px;
-}
 </style>
