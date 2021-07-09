96571f42a08c9e9ea23e79399c92cf95dfe941d0
zhaozhenghao <975106543@qq.com>
2021-07-09 18:00:27 +0800

    axios

--- react/antd-demo/src/App.js
+++ react/antd-demo/src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
+import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Redirect} from 'react-router-dom'
 import Nav from './views/Nav/Nav';
 import Home from './views/Home/home';
 import ClassList from './views/ClassList/ClassList';
+import axios from 'axios'
 /**
  * 
  * @returns Redirect 重定向 to：路径
  */
-function App() {
-  return (
-    <Router>
-      <div className="App">          
-          <Nav></Nav>
-          <Route exact path="/" render={()=><Redirect to="/home"/>}></Route>
-          <Route exact path="/home" component={Home}></Route>
-          <Route exact path="/ClassList" component={ClassList}></Route>
-      </div>
-    </Router>
-  );
-}
+ export default class App extends Component {
+  
+  componentDidMount(){
+    axios({
+      url:"http://httpbin.org/get",
+    }).then(res =>{
+      console.log('httpbin数据',res)
+    })
+  }
 
-export default App;
+  render(){
+    return (
+      <Router>
+        <div className="App">          
+            <Nav></Nav>
+            <Route exact path="/" render={()=><Redirect to="/home"/>}></Route>
+            <Route exact path="/home" component={Home}></Route>
+            <Route exact path="/ClassList" component={ClassList}></Route>
+        </div>
+      </Router>
+    )
+  }
+};
